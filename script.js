e1db41742d2bd705917630f1ffb4ec523e55068a
Martino888 <maxmartino33@gmail.com>
2022-03-03 20:04:34 +0100

    click color bomb red

--- script.js
+++ script.js
@@ -5,24 +5,45 @@
 // con difficoltà 3 => tra 1 e 49
 
 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati -
+//  abbiamo calpestato una bomba - 
+//  la cella si colora di rosso e la partita termina, 
+//  altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+
+
 const livel = document.querySelector('#Difficolta')
 const btn = document.querySelector('#Play')
 const container = document.querySelector('.container');
+let bombeList; 
 
 btn.addEventListener('click', function() {
     container.innerHTML = ''
     if(livel.value == 'Easy'){
-        for (let i = 1; i <= 49; i++){
+        bombe(49);
+        for (let i = 0; i < 49; i++){
             let item = document.createElement('div');
             item.classList.add('square');
             item.classList.add('easy')
             item.innerHTML = i;
             container.append(item);
             item.addEventListener('click', function(){
-                item.classList.add('select-quadrato')
+                for(j = 0; j < bombeList.length; j++){
+                    if(i == bombeList[j]){
+                        console.log('hai perso');
+                        item.classList.add('bomb');
+                    }else{
+                        item.classList.add('select-quadrato');
+                        console.log('continua cosi');
+                    } 
+                }
             })
         }
     }else if(livel.value == 'Medium'){
+        bombe(81)
         for (let i = 1; i <= 81; i++){
             let item = document.createElement('div');
             item.classList.add('square');
@@ -30,10 +51,19 @@ btn.addEventListener('click', function() {
             item.innerHTML = i;
             container.append(item);
             item.addEventListener('click', function(){
-                item.classList.add('select-quadrato')
+                for(j = 0; j < bombeList.length; j++){
+                    if(i == bombeList[j]){
+                        console.log('hai perso');
+                        item.classList.add('bomb');
+                    }else{
+                        item.classList.add('select-quadrato');
+                        console.log('continua cosi');
+                    } 
+                }
             })
         }
     }else{
+        bombe(100)
         for (let i = 1; i <= 100; i++){
             let item = document.createElement('div');
             item.classList.add('square');
@@ -41,8 +71,31 @@ btn.addEventListener('click', function() {
             item.innerHTML = i;
             container.append(item);
             item.addEventListener('click', function(){
-                item.classList.add('select-quadrato')
+                for(j = 0; j < bombeList.length; j++){
+                    if(i == bombeList[j]){
+                        console.log('hai perso');
+                        item.classList.add('bomb');
+                    }else{
+                        item.classList.add('select-quadrato');
+                        console.log('continua cosi');
+                    } 
+                }
             })
         }
     }
+    console.log(bombeList)
 })
+
+
+
+
+function bombe(max){
+    bombeList = [];
+    while(bombeList.length < 16){
+    let randomNumber = Math.floor(Math.random() * max);
+    console.log(randomNumber);
+    if(!bombeList.includes(randomNumber)){
+        bombeList.push(randomNumber);
+    }
+    }
+}
